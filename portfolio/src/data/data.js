1e0fc08bab5c2382c3ee605493b7824c57a3d3e4
MilanPavasiya <milpavasiya@gmail.com>
2025-10-14 11:42:14 -0400

    resume link updated

--- portfolio/src/data/data.js
+++ portfolio/src/data/data.js
@@ -10,7 +10,7 @@ export const Bio = {
 		'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
 	github: 'https://github.com/MilanPavasiya',
 	resume:
-		'https://drive.google.com/file/d/1iXCqfo2NIoVcoE4DrAcH8_wR5QouIrEs/view?usp=sharing',
+		'https://drive.google.com/file/d/1Sep7DZI9e478J5503B1kNLFVGtAg6gE7/view?usp=sharing',
 	linkedin: 'https://www.linkedin.com/in/milan-pavasiya/',
 	leetcode: 'https://leetcode.com/u/MilanPavasiya/',
 };
